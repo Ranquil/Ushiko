3575ac0f5580c96bef7da3e661a23d5c927f8726
Jere Oikarinen <oikarinenjere@gmail.com>
2015-10-09 08:17:36 +0300

    Added missing files to project

--- Siika2D/Siika2D/Tegra-Android/Debug/bin/Siika2D-debug-unaligned.apk.d
+++ Siika2D/Siika2D/Tegra-Android/Debug/bin/Siika2D-debug-unaligned.apk.d
@@ -1,4 +1,5 @@
-C:\Users\tti13saleksil\Desktop\Siika2D_syksy15\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D-debug-unaligned.apk : \
-C:\Users\tti13saleksil\Desktop\Siika2D_syksy15\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D.ap_ \
-C:\Users\tti13saleksil\Desktop\Siika2D_syksy15\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\gdbserver \
-C:\Users\tti13saleksil\Desktop\Siika2D_syksy15\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\libSiika2D.so \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D-debug-unaligned.apk : \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D.ap_ \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\classes.dex \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\gdbserver \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\libSiika2D.so \
