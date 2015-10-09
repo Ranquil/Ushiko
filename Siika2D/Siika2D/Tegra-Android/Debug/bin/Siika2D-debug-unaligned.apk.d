149a7a9578be2b66929944470fa5dc5ad44ebf5d
Jere Oikarinen <oikarinenjere@gmail.com>
2015-10-09 12:01:08 +0300

    Fix shit

--- Siika2D/Siika2D/Tegra-Android/Debug/bin/Siika2D-debug-unaligned.apk.d
+++ Siika2D/Siika2D/Tegra-Android/Debug/bin/Siika2D-debug-unaligned.apk.d
@@ -1,4 +1,5 @@
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D-debug-unaligned.apk : \
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D.ap_ \
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\gdbserver \
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\libSiika2D.so \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D-debug-unaligned.apk : \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\Siika2D.ap_ \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\classes.dex \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\gdbserver \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\libs\armeabi-v7a\libSiika2D.so \
