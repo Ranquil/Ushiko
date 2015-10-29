4a9570c506e49c1cedbbc4f06b7ac0ec35dce7c7
Jere Oikarinen <oikarinenjere@gmail.com>
2015-10-29 08:08:19 +0200

    temp

--- Siika2D/Siika2D/Tegra-Android/Debug/gen/R.java.d
+++ Siika2D/Siika2D/Tegra-Android/Debug/gen/R.java.d
@@ -1,3 +1,3 @@
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\gen\com\siika2D\R.java \
- : C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\res\values\strings.xml \
-C:\Users\JarnoLaa\Desktop\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\AndroidManifest.xml \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\gen\com\siika2D\R.java \
+ : C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\res\values\strings.xml \
+C:\Users\JereOik\Documents\GitHub\Ushiko\Siika2D\Siika2D\Tegra-Android\Debug\bin\AndroidManifest.xml \
