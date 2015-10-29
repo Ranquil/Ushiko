5522729cf23f6e9fae0974e69e454f7783a70b29
WMPerkele <jarlaaks@gmail.com>
2015-10-29 15:09:36 +0200

    aegysjfg

--- Siika2D/Siika2D/jni/MainMenu.cpp
+++ Siika2D/Siika2D/jni/MainMenu.cpp
@@ -16,8 +16,8 @@ void MainMenu::Init(core::Siika2D *siika)
 
 	misc::SpriteComponent *sprtComp = new misc::SpriteComponent(misc::SpriteComponent(siika->_spriteManager->createSprite(
 		glm::vec2(0, 0),
-		glm::vec2(128, 128),
 		glm::vec2(64, 64),
+		glm::vec2(32, 32),
 		startGameTexture,
 		glm::vec2(0, 0),
 		glm::vec2(1, 1))));
@@ -30,11 +30,15 @@ void MainMenu::Init(core::Siika2D *siika)
 
 	glm::vec2 screenSize = siika->_graphicsContext->getDisplaySize();
 	startGameButton.move(glm::vec2(screenSize.x / 2, screenSize.y / 2));
+
+	siika->_graphicsContext->setClearColor(graphics::Color(255, 0, 255, 255));
+
+	hasBeenInit = true;
 }
 
 void MainMenu::DeInit(core::Siika2D *siika)
 {
-
+	
 }
 
 int MainMenu::update(core::Siika2D *siika)
@@ -43,6 +47,7 @@ int MainMenu::update(core::Siika2D *siika)
 	{
 		Init(siika);
 	}
+	
 
 	return MAIN_MENU;
 }
