d1931aa1fa21a921149a3f7f99b93e3f8417e403
WMPerkele <jarlaaks@gmail.com>
2015-10-30 09:46:54 +0200

    Added init to LevelSelect
    Conflicts:
    	Siika2D/Siika2D.v12.suo
    	Siika2D/Siika2D/Siika2D.vcxproj.filters
    	Siika2D/Siika2D/Tegra-Android/Debug/bin/Siika2D.ap_.d
    	Siika2D/Siika2D/Tegra-Android/Debug/libs/armeabi-v7a/libSiika2D.so
    	Siika2D/Tegra-Android/Debug/libBox2D.a
    	Siika2D/Tegra-Android/Debug/libSiika2D.so
    	Siika2D/Tegra-Android/Debug/libfreetype.a
    	Siika2D/Tegra-Android/Debug/stripped_libSiika2D.so

--- Siika2D/Siika2D/jni/LevelSelect.cpp
+++ Siika2D/Siika2D/jni/LevelSelect.cpp
@@ -11,6 +11,30 @@ LevelSelect::~LevelSelect()
 
 }
 
+void LevelSelect::init(core::Siika2D *siika)
+{
+	graphics::Texture *lvlSelectTexture = siika->_textureManager->createTexture("background_plains");
+	misc::SpriteComponent *sprtComp = new misc::SpriteComponent(misc::SpriteComponent(siika->_spriteManager->createSprite(
+		glm::vec2(0, 0),
+		glm::vec2(512, 512),
+		glm::vec2(256, 256),
+		lvlSelectTexture,
+		glm::vec2(0, 0),
+		glm::vec2(0, 0))));
+	misc::TransformComponent *transComp = new misc::TransformComponent;
+
+	plainsLevel.addComponent(sprtComp);
+	plainsLevel.addComponent(transComp);
+
+
+}
+
+void LevelSelect::deInit(core::Siika2D *siika)
+{
+
+}
+
+
 int LevelSelect::update(core::Siika2D *siika)
 {
 
--- Siika2D/Siika2D/jni/LevelSelect.hpp
+++ Siika2D/Siika2D/jni/LevelSelect.hpp
@@ -8,7 +8,15 @@ public:
 	LevelSelect();
 	~LevelSelect();
 
+
+	virtual void init(core::Siika2D *siika);
+	virtual void deInit(core::Siika2D *siika);
+
 	virtual int update(core::Siika2D *siika);
+
+	misc::GameObject plainsLevel;
+	misc::GameObject forestLevel;
+	misc::GameObject castleLevel;
 };
 
 
