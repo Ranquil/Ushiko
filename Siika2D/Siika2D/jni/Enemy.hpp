0477ddef10c197d7d50abe6c111a6056689daadf
Jere Oikarinen <oikarinenjere@gmail.com>
2015-11-04 14:58:29 +0200

    Enemy hpp edits

--- Siika2D/Siika2D/jni/Enemy.hpp
+++ Siika2D/Siika2D/jni/Enemy.hpp
@@ -3,6 +3,7 @@
 
 #include "../engine/core/Siika2D.h"
 #include "../engine/misc/GameObject.h"
+#include "../engine/misc/Timer.h"
 
 class Enemy
 {
@@ -14,8 +15,10 @@ public:
 	void deInit(core::Siika2D *siika);
 	void update(core::Siika2D *siika);
 
+private:
 	misc::GameObject *go;
 	std::string enemyTextureName;
+	misc::Timer animationTimer;
 };
 
 #endif // ENEMY_H
