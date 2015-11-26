cad3199ca2b0430e12c8f598d31c820eb9be2e51
Jere Oikarinen <oikarinenjere@gmail.com>
2015-11-26 12:02:13 +0200

    Fixed github shit

--- Siika2D/Siika2D/jni/LevelSelect.cpp
+++ Siika2D/Siika2D/jni/LevelSelect.cpp
@@ -16,18 +16,6 @@ LevelSelect::~LevelSelect()
 
 void LevelSelect::init(core::Siika2D *siika)
 {
-<<<<<<< HEAD
-	//misc::File *file = siika->getFile("progress.txt");
-	//file->writeFile("0");
-	//std::string read = file->readFile();
-
-	//if (read.find("1") != std::string::npos) unlocked = 1;
-	//else if (read.find("2") != std::string::npos) unlocked = 2;
-	//else if (read.find("3") != std::string::npos) unlocked = 3;
-	//else unlocked = 0;
-
-	unlocked = 3;
-=======
 	misc::File *file = siika->getFile("progress.txt");
 	std::string read = file->readFile();
 
@@ -35,7 +23,6 @@ void LevelSelect::init(core::Siika2D *siika)
 	if (read.find("1") != std::string::npos) unlocked = 1;
 	if (read.find("2") != std::string::npos) unlocked = 2;
 	if (read.find("3") != std::string::npos) unlocked = 3;
->>>>>>> origin/master
 
 	glm::vec2 screenSize = siika->transfCrds()->deviceToUser(siika->_graphicsContext->getDisplaySize());
 	boxSizex = siika->_graphicsContext->getDisplaySize().x / 4;
